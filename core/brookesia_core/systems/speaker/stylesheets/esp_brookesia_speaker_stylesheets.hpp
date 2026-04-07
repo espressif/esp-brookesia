38c3cd56696e79baf77e6313d98e71b3905c8e68
Liu Zhongwei <liuzhongwei@espressif.com>
2026-04-07 14:56:37 +0800

    feat(repo): remove core and other components of the old structure

--- core/brookesia_core/systems/speaker/stylesheets/esp_brookesia_speaker_stylesheets.hpp
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-#pragma once
-
-/* Speaker */
-#include "360x360/dark/stylesheet.hpp"
